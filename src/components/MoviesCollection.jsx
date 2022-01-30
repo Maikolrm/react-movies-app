d30ff01eb1b97fe983dfbb93c8d603831a53939b
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-01-30 19:15:55 -0400

    Components - MoviesCollection - using Boolean function to set grid property value bases on movies.length

--- src/components/MoviesCollection.jsx
+++ src/components/MoviesCollection.jsx
@@ -6,8 +6,8 @@ import Movie from "./Movie"
 
 function MoviesCollection({ movies, collection }) {
   return (
-    <Page title={`${collection} Movies`} grid={true} >
-      {Boolean(movies.length) ? movies.map(movie=> <Movie movie={movie} remove={true} collection={collection} />) : <h1 className="mt-10 text-[16px] text-center text-gray-400 leading-none tracking-wide">Your <span className="font-semibold">{collection}</span> movies list is empty. Add some movies</h1>}
+    <Page title={`${collection} Movies`} grid={Boolean(movies.length)} >
+      {Boolean(movies.length) ? movies.map(movie=> <Movie movie={movie} remove={true} collection={collection} />) : <h1 className="text-[16px] text-center text-gray-400 leading-none tracking-wide">Your <span className="font-semibold">{collection}</span> movies list is empty. Add some movies</h1>}
     </Page>
   )
 }
