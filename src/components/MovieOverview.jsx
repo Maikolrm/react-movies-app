01cdbfc6abb7e0628df877a2fd3eea60e4b33b34
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-01-30 17:12:35 -0400

    Components - MovieOverview - removing unnecesary wrapper div

--- src/components/MovieOverview.jsx
+++ src/components/MovieOverview.jsx
@@ -58,36 +58,34 @@ function MovieOverview(props) {
 
   return (
     <Page title={`${state.movie.title ? state.movie.title + ' - Overview': '...'}`} >
-      <div className="py-12">
-        <div className="bg-gray-100 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(https://themoviedb.org/t/p/w1280${state.movie.backdrop_path})` } }>
-          <div className="bg-black/60 p-[2vw] flex">
-            <div className="w-[20vw] max-w-[400px] rounded-lg overflow-hidden shadow-lg">
-              <img className="w-full h-full object-cover" src={`https://themoviedb.org/t/p/w400${state.movie.poster_path}`} alt={state.movie.title} />
+      <div className="bg-gray-100 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(https://themoviedb.org/t/p/w1280${state.movie.backdrop_path})` } }>
+        <div className="bg-black/60 p-[2vw] flex">
+          <div className="w-[20vw] max-w-[400px] rounded-lg overflow-hidden shadow-lg">
+            <img className="w-full h-full object-cover" src={`https://themoviedb.org/t/p/w400${state.movie.poster_path}`} alt={state.movie.title} />
+          </div>
+          <div className="flex flex-1 flex-col justify-center px-[2vw] pr-[20vw]">
+            <h1 className="font-semibold text-[1.5vw] text-gray-100 leading-none">{state.movie.title}</h1>
+            {/* ACTIONS */}
+            <div className="flex items-center py-6">
+              <button onClick={() => handleAction('watched')} className={"w-10 h-10 bg-sky-900 rounded-full text-sm leading-10 " + (isWatched ? 'text-sky-500' : 'text-white')}><i className="fas fa-tasks"></i></button>
+              <button onClick={() => handleAction('favorites')} className={"w-10 h-10 ml-3 bg-sky-900 rounded-full text-sm leading-10 " + (isFavorite ? 'text-sky-500' : 'text-white')}><i className="fas fa-heart"></i></button>
+              {/* <button className="w-10 h-10 ml-3 bg-sky-900 rounded-full text-white text-sm leading-10"><i className="fas fa-star"></i></button> */}
+              <button className="ml-5 font-bold text-white leading-none">view trailer</button>
             </div>
-            <div className="flex flex-1 flex-col justify-center px-[2vw] pr-[20vw]">
-              <h1 className="font-semibold text-[1.5vw] text-gray-100 leading-none">{state.movie.title}</h1>
-              {/* ACTIONS */}
-              <div className="flex items-center py-6">
-                <button onClick={() => handleAction('watched')} className={"w-10 h-10 bg-sky-900 rounded-full text-sm leading-10 " + (isWatched ? 'text-sky-500' : 'text-white')}><i className="fas fa-tasks"></i></button>
-                <button onClick={() => handleAction('favorites')} className={"w-10 h-10 ml-3 bg-sky-900 rounded-full text-sm leading-10 " + (isFavorite ? 'text-sky-500' : 'text-white')}><i className="fas fa-heart"></i></button>
-                {/* <button className="w-10 h-10 ml-3 bg-sky-900 rounded-full text-white text-sm leading-10"><i className="fas fa-star"></i></button> */}
-                <button className="ml-5 font-bold text-white leading-none">view trailer</button>
-              </div>
-              {/* ACTIONS */}
-              <h2 className="text-sm text-gray-300 italic tracking-wide">{state.movie.tagline}</h2>
-              <div className="mt-4">
-                <h3 className="font-bold text-xl text-white leading-none">Vista general</h3>
-                <p className="mt-4 text-white leading-8 tracking-wide">{state.movie.overview}</p>
-              </div>
-              <div className="mt-6">
-                <h3 className="font-semibold text-md text-white leading-none">{state.director.name}</h3>
-                <p className="mt-2 text-xs text-white tracking-wide">{state.director.job}</p>
-              </div>
+            {/* ACTIONS */}
+            <h2 className="text-sm text-gray-300 italic tracking-wide">{state.movie.tagline}</h2>
+            <div className="mt-4">
+              <h3 className="font-bold text-xl text-white leading-none">Vista general</h3>
+              <p className="mt-4 text-white leading-8 tracking-wide">{state.movie.overview}</p>
+            </div>
+            <div className="mt-6">
+              <h3 className="font-semibold text-md text-white leading-none">{state.director.name}</h3>
+              <p className="mt-2 text-xs text-white tracking-wide">{state.director.job}</p>
             </div>
           </div>
         </div>
-        {Boolean(state.cast.length) && <MovieCast cast={state.cast} />}
       </div>
+      {Boolean(state.cast.length) && <MovieCast cast={state.cast} />}
     </Page>
   )
 }
