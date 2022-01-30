802dad3fff763bf2c9d1d85d39f5c80a1093118c
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-01-30 17:15:51 -0400

    Components - MoviesCollection - passing new propeerty to Page component ( grid )

--- src/components/MoviesCollection.jsx
+++ src/components/MoviesCollection.jsx
@@ -6,8 +6,8 @@ import Movies from "./Movies"
 
 function MoviesCollection({ movies, collection }) {
   return (
-    <Page title={`${collection} Movies`}>
-      <Movies movies={movies} remove={true} collection={collection} />
+    <Page title={`${collection} Movies`} grid={true} >
+      {Boolean(movies.length) ? <Movies movies={movies} remove={true} collection={collection} /> : <h1 className="mt-10 text-[16px] text-center text-gray-400 leading-none tracking-wide">Your <span className="font-semibold">{collection}</span> movies list is empty. Add some movies</h1>}
     </Page>
   )
 }
