892a95b437a09d96b8950345958429135e4fee08
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-01-30 13:05:41 -0400

    Components - Movies - passing new component prop ( remove )

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,10 +3,10 @@ import React, { useEffect } from "react"
 // COMPONENTS
 import Movie from './Movie'
 
-function Movies({ movies }) {
+function Movies({ movies, remove }) {
   return (
     <div className="px-2 py-10 max-w-screen-2xl m-auto grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      {movies.map(movie => <Movie key={movie.id} movie={movie} remove={remove} />)}
     </div>
   )
 }
