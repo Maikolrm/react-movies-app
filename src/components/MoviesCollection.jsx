49eb60d9aa14f04055f3fc5692485c287df0c515
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-01-30 17:21:26 -0400

    Components - MoviesCollection - fixing error related to Movies component ( it was deleted )

--- src/components/MoviesCollection.jsx
+++ src/components/MoviesCollection.jsx
@@ -2,12 +2,12 @@ import React, { useEffect } from "react"
 
 // COMPONENTS
 import Page from "./Page"
-import Movies from "./Movies"
+import Movie from "./Movie"
 
 function MoviesCollection({ movies, collection }) {
   return (
     <Page title={`${collection} Movies`} grid={true} >
-      {Boolean(movies.length) ? <Movies movies={movies} remove={true} collection={collection} /> : <h1 className="mt-10 text-[16px] text-center text-gray-400 leading-none tracking-wide">Your <span className="font-semibold">{collection}</span> movies list is empty. Add some movies</h1>}
+      {Boolean(movies.length) ? movies.map(movie=> <Movie movie={movie} remove={true} collection={collection} />) : <h1 className="mt-10 text-[16px] text-center text-gray-400 leading-none tracking-wide">Your <span className="font-semibold">{collection}</span> movies list is empty. Add some movies</h1>}
     </Page>
   )
 }
