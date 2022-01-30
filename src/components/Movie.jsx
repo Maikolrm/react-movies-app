ca069f0fd7574ae2c86c93941beda495ee8e2ca3
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-01-30 14:43:14 -0400

    Components - Movie - removing movie from collections ( favorites & wathed )

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect } from "react"
+import React, { useContext } from "react"
 import { Link } from 'react-router-dom'
 
-function Movie({ movie, remove }) {
+// CONTEXT
+import AppDispatch from "../AppDispatch"
+
+function Movie({ movie, remove, collection }) {
+
+  // APP DISPATCH
+  const appDispatch = useContext(AppDispatch)
+
+  // HANDLE REMOVE
+  function handleRemove(id) {
+    appDispatch({ type: 'toggle-collections', isInCollection: true, collection: collection, movie: { id: id } })
+  }
+
   return (
     <div className="relative p-1 bg-white rounded-sm border overflow-hidden">
-      {remove && <button className="absolute top-3 left-[50%] -ml-5 z-10 w-10 h-10 bg-gray-900/50 rounded text-sm text-center text-red-400 leading-10"><i className="fas fa-trash"></i></button>}
+      {remove && <button onClick={() => handleRemove(movie.id)} className="absolute top-3 left-[50%] -ml-5 z-10 w-10 h-10 bg-gray-900/50 rounded text-sm text-center text-red-400 leading-10"><i className="fas fa-trash"></i></button>}
       <Link to={`/movie-overview/${movie.id}`} className="block max-h-[550px] bg-gray-200 rounded overflow-hidden sm:h-[70vw] md:h-[50vw]">
         {movie.poster_path ? <img  className="w-full h-full object-cover" src={"https://themoviedb.org/t/p/w300" + (movie.poster_path)} alt={movie.title} /> : ''}
       </Link>
