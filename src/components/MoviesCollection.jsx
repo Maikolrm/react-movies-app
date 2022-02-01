e6166accb0122bada062c5f02440d58632c8fc23
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-01 12:02:45 -0400

    Components - MoviesCollection - importing and rendering ContentHint component

--- src/components/MoviesCollection.jsx
+++ src/components/MoviesCollection.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react"
+import React from "react"
 
 // COMPONENTS
 import Page from "./Page"
 import Movie from "./Movie"
+import ContentHint from "./ContentHint"
 
 function MoviesCollection({ movies, collection }) {
   return (
     <Page title={`${collection} Movies`} grid={Boolean(movies.length)} >
-      {Boolean(movies.length) ? movies.map(movie=> <Movie movie={movie} remove={true} collection={collection} />) : <h1 className="text-[16px] text-center text-gray-400 leading-none tracking-wide">Your <span className="font-semibold">{collection}</span> movies list is empty. Add some movies</h1>}
+      {Boolean(movies.length) ? movies.map(movie=> <Movie movie={movie} remove={true} collection={collection} />) : <ContentHint content={<>Your <span className="font-bold">{collection}</span> movies list is empty. Add some movies.</>} />}
     </Page>
   )
 }
