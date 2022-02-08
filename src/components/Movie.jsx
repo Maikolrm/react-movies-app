f003011e285f4902ca9b411bdbd90cb6fa673779
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-08 11:48:42 -0400

    Components - Movie - styling component in dark mode

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -15,14 +15,14 @@ function Movie({ movie, remove, collection }) {
   }
 
   return (
-    <div className="relative p-1 bg-white rounded-sm overflow-hidden">
+    <div className="relative p-1 bg-white rounded-sm overflow-hidden dark:bg-gray-700">
       {remove && <button onClick={() => handleRemove(movie.id)} className="absolute top-3 left-[50%] -ml-5 z-10 w-10 h-10 bg-gray-900/50 rounded text-sm text-center text-red-400 leading-10 outline-none"><i className="fas fa-trash"></i></button>}
-      <Link to={`/movie-overview/${movie.id}`} className="block max-h-[550px] bg-gray-200 rounded overflow-hidden sm:h-[70vw] md:h-[50vw] outline-none">
+      <Link to={`/movie-overview/${movie.id}`} className="block max-h-[550px] bg-gray-200 rounded overflow-hidden sm:h-[70vw] md:h-[50vw] outline-none dark:bg-gray-800">
         {movie.poster_path ? <img  className="w-full h-full object-cover" src={"https://themoviedb.org/t/p/w300" + (movie.poster_path)} alt={movie.title} /> : ''}
       </Link>
       <div className="pt-3">
-        <h2 className="text-md leading-none text-gray-600 font-bold truncate">{movie.title}</h2>
-        <h4 className="mt-2 text-xs font-semibold leading-none text-gray-400">{movie.release_date ? movie.release_date : '-'}</h4>
+        <h2 className="text-md leading-none text-gray-600 font-bold truncate dark:text-gray-300">{movie.title}</h2>
+        <h4 className="mt-2 text-xs font-semibold leading-none text-gray-400 dark:text-gray-400">{movie.release_date ? movie.release_date : '-'}</h4>
       </div>
     </div>
   )
