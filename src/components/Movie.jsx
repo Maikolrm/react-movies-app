29bad2756e56ba984f95a2c0ebd91bacb71bcba6
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-10 11:24:16 -0400

    Components - Movie - importing and rendering VoteAverage component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom'
 // CONTEXT
 import AppDispatch from "../AppDispatch"
 
-function Movie({ movie, remove, collection }) {
+// COMPONENTS
+import VoteAverage from "./VoteAverage"
 
-  console.log(movie)
+function Movie({ movie, remove, collection }) {
 
   // APP DISPATCH
   const appDispatch = useContext(AppDispatch)
@@ -23,11 +24,7 @@ function Movie({ movie, remove, collection }) {
         {movie.poster_path ? <img  className="w-full h-full object-cover" src={"https://themoviedb.org/t/p/w300" + (movie.poster_path)} alt={movie.title} /> : ''}
       </Link>
       <div className="relative pt-6">
-        <div className="absolute p-[2.5px] w-10 h-10 bg-gray-300 dark:bg-gray-800 right-[5px] top-0 translate-y-[-50%] rounded-full" style={{ backgroundImage: `conic-gradient(steelblue ${movie.vote_average * 10}%, transparent 0%)` }}>
-          <div className="flex h-full bg-gray-100 dark:bg-gray-900 rounded-full">
-            <h4 className="m-auto text-xs text-gray-400 font-bold">{movie.vote_average}</h4>
-          </div>
-        </div>
+        <VoteAverage movie={movie} absolute={true} position="absolute right-[5px] top-0 translate-y-[-50%]" />
         <h2 className="text-md leading-none text-gray-600 font-bold truncate dark:text-gray-300">{movie.title}</h2>
         <h4 className="mt-2 text-xs font-semibold leading-none text-gray-400 dark:text-gray-400">{movie.release_date ? movie.release_date : '-'}</h4>
       </div>
