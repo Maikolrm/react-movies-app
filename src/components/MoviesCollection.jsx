f5b2c8c0105b592680511bf5c02be0e5385d440d
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-10 15:55:03 -0400

    Components - MoviesCollection - passing new properties to Page ( heading and count )

--- src/components/MoviesCollection.jsx
+++ src/components/MoviesCollection.jsx
@@ -7,8 +7,8 @@ import ContentHint from "./ContentHint"
 
 function MoviesCollection({ movies, collection }) {
   return (
-    <Page title={`${collection} Movies`} grid={Boolean(movies.length)} >
-      {Boolean(movies.length) ? movies.map(movie=> <Movie movie={movie} remove={true} collection={collection} />) : <ContentHint content={<>Your <span className="font-bold">{collection}</span> movies list is empty. Add some movies.</>} />}
+    <Page title={`${collection} Movies`} grid={Boolean(movies.length)} count={movies.length} heading={collection}>
+      {Boolean(movies.length) ? movies.map(movie=> <Movie key={movie.id} movie={movie} remove={true} collection={collection} />) : <ContentHint content={<>Your <span className="font-bold">{collection}</span> movies list is empty. Add some movies.</>} />}
     </Page>
   )
 }
