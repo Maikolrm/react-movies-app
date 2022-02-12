17635f56beaafdb00634c74f0fdabe496bfac4da
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-12 18:25:51 -0400

    Components - Haeder - looping through searches movies results

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,21 +58,13 @@ function Header(props) {
           </button>
         </form>
         {/* MOVIES SUGGESTIONS */}
-        <div className="hidden absolute z-10 mt-3 left-0 w-full h-60 p-1 bg-white rounded shadow-lg">
-          <a href="#" className="mb-1 flex items-center p-1 bg-gray-100 rounded outline-none hover:bg-sky-200 focus:bg-sky-200">
-            <div className="w-12 h-12 bg-white rounded overflow-hidden">
-              <img className="w-full h-full object-cover"src="https://image.tmdb.org/t/p/w200/45YC86aMjWlcPkvgWwqdI6Clq7b.jpg" alt="Last Bold" />
-            </div>
-            <h2 className="flex-1 pl-4 text-xs text-gray-600 leading-none">Movie name</h2>
-            <span className="w-8 h-8 bg-sky-200"></span>
-          </a>
-          <a href="#" className="flex items-center p-1 bg-gray-100 rounded outline-none hover:bg-sky-200 focus:bg-sky-200">
-            <div className="w-12 h-12 bg-white rounded overflow-hidden">
-              <img className="w-full h-full object-cover"src="https://image.tmdb.org/t/p/w200/pzPdwOitmTleVE3YPMfIQgLh84p.jpg" alt="Last Bold" />
-            </div>
-            <h2 className="flex-1 pl-4 text-xs text-gray-600 leading-none">Movie name</h2>
-            <span className="w-8 h-8 bg-sky-200"></span>
-          </a>
+        <div className={`absolute z-10 mt-3 left-0 w-full p-1 bg-white rounded shadow-lg ${appState.searches.show ? '' : 'hidden'}`}>
+          {appState.searches.results.map((query, i) => (
+            <Link key={i} to={`/search-movies/${query}`} className={`flex items-center ${i ? 'mt-1' : ''} bg-gray-100 outline-none rounded-sm text-gray-500 hover:bg-sky-500 hover:text-white focus:bg-sky-500 focus:text-white`}>
+              <span className="inline-block w-10 h-10 text-xs text-center leading-10"><i className="fas fa-clock"></i></span>
+              <h2 className="flex-1 font-bold uppercase text-[11px] leading-none tracking-widest">{query}</h2>
+            </Link>
+          ))}
         </div>
         {/* MOVIES SUGGESTIONS */}
       </div>
