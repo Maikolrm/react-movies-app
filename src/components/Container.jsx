695b8fe0431b0c266f7229e39ab37cd243656df3
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-14 19:37:21 -0400

    Components - Container - changing transition duration

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -13,11 +13,11 @@ function Container(props) {
   const appDispatch = useContext(AppDispatch)
 
   return (
-    <div className={`py-10 m-auto ${props.grid ? 'px-2 max-w-screen-2xl' : ''} ${appState.blured ? 'opacity-5' : ''} transition duration-[50ms]`}>
+    <div className={`py-10 m-auto ${props.grid ? 'px-2 max-w-screen-2xl' : ''} ${appState.blured ? 'opacity-5' : ''} transition duration-[100ms]`}>
       {Boolean(props.count) && <h2 className="pb-4 text-[10px] font-semibold text-gray-500 dark:text-gray-300 leading-none uppercase tracking-widest">{props.heading} <span>( {props.count} )</span></h2>}
       <div className={`${props.grid ? 'grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4' : ''}`}>
         {props.children}
-        {appState.blured && <div onClick={() => appDispatch({ type: 'show-menu', value: false })} className="fixed inset-0 z-10 w-full h-full"></div>}
+        {appState.blured && <div onClick={() => appDispatch({ type: 'show-menu', value: false })} className="fixed inset-0 z-[100] w-full h-full"></div>}
       </div>
     </div>
   )
