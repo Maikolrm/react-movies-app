697aadebcc14589ba829416cd6e6aada2e97973e
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-15 15:14:22 -0400

    Project - api - refactoring handleSearches method

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // HANDLE SEARCHES
 export const handleSearches = (searches, query) => {
-  const count = searches.length
   const exist = searches.find(search => search == query)
-  if (count > 5) searches.pop()
-  return !exist ? [query,...searches] : searches
+  if (!exist) searches = [query, ...searches] 
+  if (searches.length > 6) searches.pop()
+  return searches
 }
