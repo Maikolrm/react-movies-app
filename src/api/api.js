d42adec8eb2eefdab2d328a3777420ebc3bbb292
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-16 10:18:41 -0400

    Project - api - refactoring handleSearches method to add and delete querys

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 // HANDLE SEARCHES
-export const handleSearches = (searches, query) => {
-  const exist = searches.find(search => search == query)
-  if (!exist) searches = [query, ...searches] 
-  if (searches.length > 6) searches.pop()
+export const handleSearches = (searches, query, action) => {
+  if (action == 'new') {
+    const exist = searches.find(search => search == query)
+    if (!exist) searches = [query, ...searches] 
+    if (searches.length > 6) searches.pop()
+  } else {
+    searches = searches.filter(search => search != query)
+  }
   return searches
 }
