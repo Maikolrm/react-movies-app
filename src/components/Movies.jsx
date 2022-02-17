a0771811aad647c11b32363f050ace69fd4bb649
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-16 23:05:03 -0400

    Components - Movies - creating local variables using appState

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 import axios from "axios"
 
 // CONTEXT
+import AppState from "../AppState"
 import AppDispatch from "../AppDispatch"
 
 // COMPONENTS
@@ -14,16 +15,21 @@ import ContentHint from "./ContentHint"
 // API
 import { handleSearches } from "../api/api"
 
-function Movies(props) {
+function Movies({ page }) {
   
-  // LOCAL VARIABLES
-  const searchResults = props.searches ? props.searches.results : null
-
-  // URL PARAMS
-  const params = useParams()
+  // APP STATE
+  const appState = useContext(AppState)
 
   // APP DISPATCH
   const appDispatch = useContext(AppDispatch)
+
+  // LOCAL VARIABLES
+  const movies = appState.movies
+  const query = page == 'welcome' ? 'john wick' : appState.query
+  const searchResults = page == 'welcome' ? undefined : appState.searches.results
+
+  // URL PARAMS
+  const params = useParams()
 
   // FIRST MOUNT
   useEffect(() => appDispatch({ type: 'show-menu', value: false }), [])
@@ -36,22 +42,22 @@ function Movies(props) {
   // WATCH REQUEST COUNT CHANGES
   useEffect(() => {
     const request = axios.CancelToken.source()
-    if (props.query) {
+    if (query) {
       async function fecthData() {
         try {
-          const { data } = await axios.get(`/search/movie?api_key=${import.meta.env.VITE_APP_MDB_KEY}&query=${props.query}`)
+          const { data } = await axios.get(`/search/movie?api_key=${import.meta.env.VITE_APP_MDB_KEY}&query=${query}`)
           appDispatch({ type: 'set-movies', movies: data.results })
-          searchResults && data.results.length ? appDispatch({ type: 'set-searches', searches: handleSearches(searchResults, props.query, 'new') }) : null
+          searchResults && data.results.length ? appDispatch({ type: 'set-searches', searches: handleSearches(searchResults, query, 'new') }) : null
         } catch(e) { console.log(e) }
       }
       fecthData()
     }
     return () => request.cancel()
-  }, [props.query])
+  }, [query])
 
   return (
-    <Page title={props.query} grid={Boolean(props.movies.length)} count={props.movies.length} heading="movies">
-      {Boolean(props.movies.length) ? props.movies.map(movie => <Movie key={movie.id} movie={movie} />) : <ContentHint content={<>No <span className="font-bold">results</span>. Try again.</>} />}
+    <Page title={query} grid={Boolean(movies.length)} count={movies.length} heading="movies">
+      {Boolean(movies.length) ? movies.map(movie => <Movie key={movie.id} movie={movie} />) : <ContentHint content={<>No <span className="font-bold">results</span>. Try again.</>} />}
     </Page>
   )
 }
