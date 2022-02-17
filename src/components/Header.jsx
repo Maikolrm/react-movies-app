219573cc804af6b414d8662aee29fb25a66514d0
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-16 23:01:27 -0400

    Components - Header - creating local variables using appState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,26 @@ import AppDispatch from "../AppDispatch"
 // API
 import { handleSearches } from '../api/api'
 
-function Header(props) {
+function Header() {
 
   // LOCAL STATE
   const [query, setQuery] = useState('')
 
+  // APP STATE
+  const appState = useContext(AppState)
+
+  // APP DISPATCH
+  const appDispatch = useContext(AppDispatch)
+
   // LOCAL VARIABLES
-  const showSearches = props.searches.show
-  const searchResults = props.searches.results
+  const fetching = appState.fetching
+  const showMenu = appState.showMenu
+  const theme = appState.theme
+  const searches = appState.searches
+  const showSearches = searches.show
+  const searchResults = searches.results
+  const favorites = appState.favorites
+  const watched = appState.watched
 
   // FIELD REF
   const field = useRef()
@@ -26,16 +38,10 @@ function Header(props) {
   // NAVIGATE
   const navigate = useNavigate()
 
-  // APP STATE
-  const appState = useContext(AppState)
-
-  // APP DISPATCH
-  const appDispatch = useContext(AppDispatch)
-
   // HANDLE SUBMIT
   function handleSubmit(e) {
     e.preventDefault()
-    if (query.trim() && !props.fetching) {
+    if (query.trim() && !fetching) {
       navigate('/search-movies/' + query)
       setQuery('')
       field.current.blur()
@@ -72,8 +78,8 @@ function Header(props) {
       <div className="relative z-[200] flex-1 mx-4 max-w-md bg-white rounded-full">
         <form onSubmit={handleSubmit} className="flex rounded-full overflow-hidden">
           <input ref={field} onFocus={() => appDispatch({ type: 'show-searches', value: Boolean(searchResults.length) })} value={query} onChange={e => setQuery(e.target.value)} type="text" className="flex-1 pl-5 text-xs text-gray-500 leading-10 tracking-widest outline-none" placeholder="Search...." />
-          <button tabIndex="-1" disbled={props.fetching ? 'disabled' : ''} className={"w-10 h-10 text-[14px] text-center leading-10 outline-none " + (props.fetching ? 'text-sky-500' : 'text-gray-400')}>
-            <i className={"fas " +  (props.fetching ? 'fa-circle-notch animate-spin' : 'fa-search')}></i>
+          <button tabIndex="-1" disbled={fetching ? 'disabled' : ''} className={"w-10 h-10 text-[14px] text-center leading-10 outline-none " + (fetching ? 'text-sky-500' : 'text-gray-400')}>
+            <i className={"fas " +  (fetching ? 'fa-circle-notch animate-spin' : 'fa-search')}></i>
           </button>
         </form>
         {/* MOVIES SUGGESTIONS */}
@@ -89,19 +95,19 @@ function Header(props) {
         {/* MOVIES SUGGESTIONS */}
       </div>
       <div className="relative z-[200]">
-        <button ref={button} onClick={() => appDispatch({ type: 'show-menu', value: !appState.showMenu })} className="block w-10 h-10 border-2 border-gray-400 rounded-full focus:border-teal-600 outline-none overflow-hidden">
+        <button ref={button} onClick={() => appDispatch({ type: 'show-menu', value: !showMenu })} className="block w-10 h-10 border-2 border-gray-400 rounded-full focus:border-teal-600 outline-none overflow-hidden">
           <img src="https://www.gravatar.com/avatar/4c47390a9b7da357a0d6d051bbf66270?s=200" alt="Maikol Hernandez" />
         </button>
-        {appState.showMenu && (
+        {showMenu && (
           <div className="absolute right-0 w-[300px] p-1 mt-3 bg-white rounded shadow-md">
-            <button onClick={() => appDispatch({ type: 'toggle-theme', theme: appState.theme })} className="block w-full p-2 bg-gray-200 rounded text-xs text-left text-gray-400 outline-none">
-              {appState.theme == 'light' ? 'Dark Mode' : 'Light Mode' }
+            <button onClick={() => appDispatch({ type: 'toggle-theme', theme: theme })} className="block w-full p-2 bg-gray-200 rounded text-xs text-left text-gray-400 outline-none">
+              {theme == 'light' ? 'Dark Mode' : 'Light Mode' }
               <span className="inline-block float-right">
-                <i className={`fas ${appState.theme == 'dark' ? 'text-orange-400 fa-sun' : 'text-sky-500 fa-moon'}`}></i>
+                <i className={`fas ${theme == 'dark' ? 'text-orange-400 fa-sun' : 'text-sky-500 fa-moon'}`}></i>
               </span>
             </button>
-            <Link onClick={() => appDispatch({ type: 'show-menu', value: false })} to="/favorites" className="block p-2 mt-1 hover:bg-gray-100 text-xs text-gray-400 outline-none focus:bg-gray-100">Fovorite movies <span className="float-right inline-block pr-1">{appState.favorites.length}</span></Link>
-            <Link onClick={() => appDispatch({ type: 'show-menu', value: false })} to="/watched" className="block p-2 mt-1 hover:bg-gray-100 text-xs text-gray-400 outline-none focus:bg-gray-100">Watched movies <span className="float-right inline-block pr-1">{appState.watched.length}</span></Link>
+            <Link onClick={() => appDispatch({ type: 'show-menu', value: false })} to="/favorites" className="block p-2 mt-1 hover:bg-gray-100 text-xs text-gray-400 outline-none focus:bg-gray-100">Fovorite movies <span className="float-right inline-block pr-1">{favorites.length}</span></Link>
+            <Link onClick={() => appDispatch({ type: 'show-menu', value: false })} to="/watched" className="block p-2 mt-1 hover:bg-gray-100 text-xs text-gray-400 outline-none focus:bg-gray-100">Watched movies <span className="float-right inline-block pr-1">{watched.length}</span></Link>
             <button className="block w-full py-2 mt-1 bg-sky-400 rounded-sm text-sm text-white outline-none hover:bg-sky-500 focus:bg-sky-500">Logout</button>
           </div>
         )}
