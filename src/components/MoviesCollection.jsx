6996376ee2962b8ea128c7f498f332f137aacc49
Hernández Maikol <maikol_hernandez_rm@hotmail.com>
2022-02-16 23:06:09 -0400

    Components - MoviesCollection - creating local variables using appState

--- src/components/MoviesCollection.jsx
+++ src/components/MoviesCollection.jsx
@@ -1,11 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
+
+// CONTEXT
+import AppState from '../AppState'
 
 // COMPONENTS
 import Page from "./Page"
 import Movie from "./Movie"
 import ContentHint from "./ContentHint"
 
-function MoviesCollection({ movies, collection }) {
+function MoviesCollection({ collection }) {
+
+  // APP STATE
+  const appState = useContext(AppState)
+
+  // LOCAL VARIABLES
+  const movies = collection == 'favorites' ? appState.favorites : appState.watched
+  
   return (
     <Page title={`${collection} Movies`} grid={Boolean(movies.length)} count={movies.length} heading={collection}>
       {Boolean(movies.length) ? movies.map(movie=> <Movie key={movie.id} movie={movie} remove={true} collection={collection} />) : <ContentHint content={<>Your <span className="font-bold">{collection}</span> movies list is empty. Add some movies.</>} />}
